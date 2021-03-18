e6d99cec327081c700a8dfe04119af7647803368
KalleChen <chen1082yo@gmail.com>
2021-03-18 22:50:39 +0800

    fix vh

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 
 import store from './store'
+import './utils/vh'
 
 ReactDOM.render(
   <Provider store={store}>
